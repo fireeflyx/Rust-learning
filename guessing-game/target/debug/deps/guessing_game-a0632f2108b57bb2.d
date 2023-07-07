2c2bffe02c07ca2ca4e806597440e31743adcfc5
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-07-07 22:45:01 +0300

    feat: create directory for the guessing game project

---
+++ guessing-game/target/debug/deps/guessing_game-a0632f2108b57bb2.d
@@ -0,0 +1,5 @@
+/Users/paveldubrovskij/Desktop/Rust-learning/guessing-game/target/debug/deps/guessing_game-a0632f2108b57bb2.rmeta: src/main.rs
+
+/Users/paveldubrovskij/Desktop/Rust-learning/guessing-game/target/debug/deps/guessing_game-a0632f2108b57bb2.d: src/main.rs
+
+src/main.rs:
---
+++ guessing-game/target/debug/deps/guessing_game-dc9d2fb607e12588.d
@@ -0,0 +1,5 @@
+/Users/paveldubrovskij/Desktop/Rust-learning/guessing-game/target/debug/deps/guessing_game-dc9d2fb607e12588.rmeta: src/main.rs
+
+/Users/paveldubrovskij/Desktop/Rust-learning/guessing-game/target/debug/deps/guessing_game-dc9d2fb607e12588.d: src/main.rs
+
+src/main.rs:

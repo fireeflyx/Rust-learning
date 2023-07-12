eb736c479eb432af94df3388def76b81c6e89f0c
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-07-12 13:12:14 +0300

    feat: create directory for the minigrep project

---
+++ simple-vector-manager/target/debug/deps/simple_vector_manager-62975c9b4356cb78.d
@@ -0,0 +1,5 @@
+/Users/paveldubrovskij/Desktop/Rust-learning/simple-vector-manager/target/debug/deps/simple_vector_manager-62975c9b4356cb78.rmeta: src/main.rs
+
+/Users/paveldubrovskij/Desktop/Rust-learning/simple-vector-manager/target/debug/deps/simple_vector_manager-62975c9b4356cb78.d: src/main.rs
+
+src/main.rs:
---
+++ simple-vector-manager/target/debug/deps/simple_vector_manager-6b8170175bd2aeed.d
@@ -0,0 +1,5 @@
+/Users/paveldubrovskij/Desktop/Rust-learning/simple-vector-manager/target/debug/deps/simple_vector_manager-6b8170175bd2aeed.rmeta: src/main.rs
+
+/Users/paveldubrovskij/Desktop/Rust-learning/simple-vector-manager/target/debug/deps/simple_vector_manager-6b8170175bd2aeed.d: src/main.rs
+
+src/main.rs:
